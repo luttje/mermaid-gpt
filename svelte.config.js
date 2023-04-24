c02e1a0a9c8e00d666541c1148e8af28392cf480
luttje <2738114+luttje@users.noreply.github.com>
2023-04-24 17:29:13 +0200

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		
+   	paths: {
+			base: process.env.NODE_ENV === "production" ? "/mermaid-gpt" : "",
+		},
+	}
+};
+
+export default config;
