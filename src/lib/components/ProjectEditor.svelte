7f2ef83845cbafeb0968b6a6e452226523ed3899
luttje <2738114+luttje@users.noreply.github.com>
2023-04-24 22:55:03 +0200

    hide api key

--- src/lib/components/ProjectEditor.svelte
+++ src/lib/components/ProjectEditor.svelte
@@ -2,6 +2,7 @@
 	import { diagram } from '$lib/stores/diagram';
   import { onMount } from 'svelte';
 
+  let apiKeyElType: string = 'password';
   let apiKey: string | undefined = undefined;
   let projectDescription: string = "A car sharing app where persons can be the driver of many cars. But both a car and a person can exist without the other. A person has human attributes. A car has car attributes.";
   let isGeneratingDiagram = false;
@@ -13,6 +14,11 @@
     }
 
     return true;
+  }
+
+  function updateApiKey(event: Event) {
+    const input = event.target as HTMLInputElement;
+    apiKey = input.value;
   }
 
   // Save the API key to local storage
@@ -43,15 +49,21 @@
   }
 
   onMount(() => {
-    apiKey = localStorage.getItem('apiKey') || undefined;
-
+    apiKey = localStorage.getItem('apiKey') || '';
   });
 </script>
 
 <div class="flex flex-col grow-0 bg-slate-400 shadow p-4 gap-4">
   <div class="flex flex-col gap-2 grow">
     <label for="apiKey" class="text-xl">OpenAI API Key:</label>
-    <input bind:value={apiKey} id="apiKey" class="flex-1 rounded-lg p-2" placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" />
+    <div class="flex flex-row gap-2">
+      <input type={ apiKeyElType } value={apiKey} id="apiKey" class="grow rounded-lg p-2" placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" on:input={(e) => updateApiKey(e)} />
+      <button class="flex flex-col justify-center items-center bg-slate-200 rounded-lg p-2" on:click={() => apiKeyElType = apiKeyElType === 'password' ? 'text' : 'password'}>
+        <svg fill="#000000" class="w-4 h-4" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 16q0.064 0.128 0.16 0.352t0.48 0.928 0.832 1.344 1.248 1.536 1.664 1.696 2.144 1.568 2.624 1.344 3.136 0.896 3.712 0.352 3.712-0.352 3.168-0.928 2.592-1.312 2.144-1.6 1.664-1.632 1.248-1.6 0.832-1.312 0.48-0.928l0.16-0.352q-0.032-0.128-0.16-0.352t-0.48-0.896-0.832-1.344-1.248-1.568-1.664-1.664-2.144-1.568-2.624-1.344-3.136-0.896-3.712-0.352-3.712 0.352-3.168 0.896-2.592 1.344-2.144 1.568-1.664 1.664-1.248 1.568-0.832 1.344-0.48 0.928zM10.016 16q0-2.464 1.728-4.224t4.256-1.76 4.256 1.76 1.76 4.224-1.76 4.256-4.256 1.76-4.256-1.76-1.728-4.256zM12 16q0 1.664 1.184 2.848t2.816 1.152 2.816-1.152 1.184-2.848-1.184-2.816-2.816-1.184-2.816 1.184l2.816 2.816h-4z"></path>
+        </svg>
+      </button>
+    </div>
   </div>
   <div class="flex flex-col gap-2 grow">
     <label for="projectDescription" class="text-xl">Describe the entities in your application and how they relate:</label>
